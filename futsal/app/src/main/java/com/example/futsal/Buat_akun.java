2d36856da838e020f4ea8c055133cea99c17794c
dimasfajrul <dimasfajrul24@.com>
2020-04-06 19:26:08 +0700

    dimas

--- futsal/app/src/main/java/com/example/futsal/Buat_akun.java
+++ futsal/app/src/main/java/com/example/futsal/Buat_akun.java
@@ -12,6 +12,14 @@ public class Buat_akun extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buat_akun);
+        TextView btn = (TextView) findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(Buat_akun.this, MainActivity.class);
+                startActivity(inte);
+            }
+        });
 
     }
-}
+    }
