8d98235fe0aa6fc60fd97342c4d35c416ab3a104
dimasfajrul <dimasfajrul24@.com>
2020-04-20 12:11:50 +0700

    dimas

---
+++ futsal/app/src/main/java/com/example/futsal/Booking.java
@@ -0,0 +1,23 @@
+package com.example.futsal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Booking extends AppCompatActivity {
+    private Spinner spNamen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceSatete) {
+        super.onCreate(savedInstanceSatete);
+        setContentView(R.layout.booking);
+
+    }
+}
+
