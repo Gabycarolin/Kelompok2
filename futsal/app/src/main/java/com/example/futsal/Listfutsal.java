0ab78da4beca1b2b98a3cb0844aae555287ef33f
dimasfajrul <dimasfajrul24@.com>
2020-05-03 22:56:55 +0700

    dimas
    layout

--- futsal/app/src/main/java/com/example/futsal/Listfutsal.java
+++ futsal/app/src/main/java/com/example/futsal/Listfutsal.java
@@ -3,7 +3,6 @@ package com.example.futsal;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -19,25 +18,29 @@ public class Listfutsal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.list_futsal);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list_view);
     jdl = new String[] {
             "Zona Futsal","Elphasindo Futsal"
     };
     ktr = new String[]{
             "Jl.Mastrip 5 Jember","Jl.Nias III Jember"
     };
-    img = new String[]{
-            Integer.toString(R.drawable.futsal1),Integer.toString(R.drawable.futsal2)
+    img = new String[] {
+                Integer.toString(R.drawable.futsal2) , Integer.toString(R.drawable.futsal2)
     };
+
     mylist=new ArrayList<HashMap<String, String>>();
     for (int i=0; i<jdl.length; i++){
         map = new HashMap<String, String>();
         map.put("judul", jdl[i]);
         map.put("keterangan",ktr[i]);
         map.put("Gambar", img[i]);
+        mylist.add(map);
     }
     adapter = new SimpleAdapter(this,mylist,R.layout.list_futsal,
             new String[]{"judul","keterangan","Gambar"}, new int []{R.id.txt_judul,(R.id.txt_keterangan),(R.id.image) });
     listView.setAdapter(adapter);
     }
+
 }
