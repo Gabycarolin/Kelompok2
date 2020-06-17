ef377fe874a629227c83699b1e4dcfffc907c4d9
ekaru04 <kumagawaekaru04@gmail.com>
2020-06-18 03:49:05 +0700

    Update v_login.php

--- application/views/auth/v_login.php
+++ application/views/auth/v_login.php
@@ -1,9 +1,8 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
